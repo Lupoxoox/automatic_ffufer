5834215cdb36ab63889ffed62cc1fe41d89fa98e
Lupoxoox <99921517+Lupoxoox@users.noreply.github.com>
2023-08-04 07:11:53 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo unzip DNS2.zip
+sudo unzip DNS.zip
+sudo unzip extension.zip
+sudo unzip wordlists.zip
+
+sudo mv DNS2 wordlists
+sudo mv DNS wordlists
+sudo mv extension wordlists
+sudo mv subdomains-top1million-110000.txt wordlists/DNS
+sudo mv ids.txt wordlists
